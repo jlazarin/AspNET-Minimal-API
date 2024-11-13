809404296f6226e4cece187b0f348aa80c1e001c
Joao Paulo Lazarin <jplazarin@gmail.com>
2024-11-13 16:33:45 -0300

    adding new MapGet

--- Program.cs
+++ Program.cs
@@ -8,6 +8,14 @@ builder.Services.AddDbContext<RangoDbContext>(
 );
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/rango/{id}", (RangoDbContext rangoDbContext, int id) =>
+{
+    return rangoDbContext.Rangos.FirstOrDefault(x => x.Id == id);
+});
+
+app.MapGet("/rangos", (RangoDbContext rangoDbContext) =>
+{
+    return rangoDbContext.Rangos;
+});
 
 app.Run();
